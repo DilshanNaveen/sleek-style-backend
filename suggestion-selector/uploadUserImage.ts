f90a4c0b8b79f12c04aa53f3531038188edbe94d
Dilshan Naveen <dilshan@vertex.lk>
2023-01-31 20:45:57 +0530

    added axios

--- suggestion-selector/uploadUserImage.ts
+++ suggestion-selector/uploadUserImage.ts
@@ -2,10 +2,11 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { getSuccessResponse, getErrorResponse } from "./utils/responseUtil";
 import { S3_METHODS, getSignedUrl } from "./utils/s3Utils";
 
-
 export const get = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
     const { key, contentType = undefined, method = S3_METHODS.get }: any = event.queryStringParameters;
+    const body = event;
+    console.log("body: ", body);
     const url = await getSignedUrl(process.env.S3_BUCKET_USER_DATA, key, method, contentType);
     return getSuccessResponse({ body: { url } });
   } catch (error) {
