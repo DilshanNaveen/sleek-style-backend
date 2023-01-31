bc2dfb15984fb12d8d2caea0834cc7eefac9e7be
Dilshan Naveen <dilshan@vertex.lk>
2023-01-31 21:56:43 +0530

    file

--- suggestion-selector/uploadUserImage.ts
+++ suggestion-selector/uploadUserImage.ts
@@ -5,9 +5,10 @@ import { S3_METHODS, getSignedUrl } from "./utils/s3Utils";
 export const get = async (event: APIGatewayProxyEvent | any): Promise<APIGatewayProxyResult> => {
   try {
     const { key, contentType = undefined, method = S3_METHODS.get }: any = event.queryStringParameters;
-    console.log("formData :", event.body);
+    const file = Buffer.from(event.body, "base64");
     // const fileType = body.query['file-type'];
     // console.log("fileType :", fileType);
+    console.log("file :", file);
     const url = await getSignedUrl(process.env.S3_BUCKET_USER_DATA, key, method, contentType);
     return getSuccessResponse({ body: { url } });
   } catch (error) {
