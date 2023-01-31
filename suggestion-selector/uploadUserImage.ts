06a25e673e3dbfbb955fe90dd1139247ba4897c8
Dilshan Naveen <dilshan@vertex.lk>
2023-01-31 21:45:34 +0530

    added form data libery

--- suggestion-selector/uploadUserImage.ts
+++ suggestion-selector/uploadUserImage.ts
@@ -2,6 +2,8 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { getSuccessResponse, getErrorResponse } from "./utils/responseUtil";
 import { S3_METHODS, getSignedUrl } from "./utils/s3Utils";
 
+const FormData = require("form-data");
+
 export const get = async (event: APIGatewayProxyEvent | any): Promise<APIGatewayProxyResult> => {
   try {
     const { key, contentType = undefined, method = S3_METHODS.get }: any = event.queryStringParameters;
