75a71cce7821d79a9494122350faca42c7612eba
Dilshan Naveen <dilshan@vertex.lk>
2023-01-31 21:39:45 +0530

    added form data

--- suggestion-selector/uploadUserImage.ts
+++ suggestion-selector/uploadUserImage.ts
@@ -2,10 +2,12 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { getSuccessResponse, getErrorResponse } from "./utils/responseUtil";
 import { S3_METHODS, getSignedUrl } from "./utils/s3Utils";
 
-export const get = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const get = async (event: APIGatewayProxyEvent | any): Promise<APIGatewayProxyResult> => {
   try {
     const { key, contentType = undefined, method = S3_METHODS.get }: any = event.queryStringParameters;
-    console.log("body = ", event);
+    console.log("body = ", event.body);
+    const formData = new FormData(event.body);
+    console.log("formData :", formData);
     // const fileType = body.query['file-type'];
     // console.log("fileType :", fileType);
     const url = await getSignedUrl(process.env.S3_BUCKET_USER_DATA, key, method, contentType);
