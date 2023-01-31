87052e2b2d04f5b2c63b700950da809f4763c67e
Dilshan Naveen <dilshan@vertex.lk>
2023-01-31 17:24:31 +0530

    added another log

--- suggestion-selector/uploadUserImage.ts
+++ suggestion-selector/uploadUserImage.ts
@@ -5,6 +5,7 @@ import { S3_METHODS, getSignedUrl } from "./utils/s3Utils";
 export const get = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
     const { key, contentType = undefined, method = S3_METHODS.get }: any = event.queryStringParameters;
+    console.log("received queryStringParameters: ", key, contentType, method);
     const url = await getSignedUrl(process.env.S3_BUCKET_USER_DATA, key, method, contentType);
     return getSuccessResponse({ body: { url } });
   } catch (error) {
