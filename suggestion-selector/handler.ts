9fdf98d718f476bb862a224ad3ee47d308bfbcaa
Dilshan Naveen <dilshan@vertex.lk>
2023-01-31 16:26:01 +0530

    some change

--- suggestion-selector/handler.ts
+++ suggestion-selector/handler.ts
@@ -3,7 +3,7 @@ import { getSuccessResponse, getErrorResponse } from "./utils/responseUtil";
 
 export const hello = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
-    return getSuccessResponse({ status: "SUCCESS" });
+    return getSuccessResponse({ status: "SUCCESS", Test: `${process.env.S3_BUCKET_USER_DATA}` });
   } catch (error) {
     return getErrorResponse(error.message);
   }
