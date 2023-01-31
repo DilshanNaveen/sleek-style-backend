a0d8feadce3242ccf02c5f75511d48aee37dffbb
Dilshan Naveen <dilshan@vertex.lk>
2023-01-31 20:01:59 +0530

    added url

--- suggestion-selector/uploadUserImage.ts
+++ suggestion-selector/uploadUserImage.ts
@@ -1,29 +1,30 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { getSuccessResponse, getErrorResponse } from "./utils/responseUtil";
 import { S3_METHODS } from "./utils/s3Utils";
-import { S3Client } from "@aws-sdk/client-s3";
-import { PutObjectCommand } from "@aws-sdk/client-s3"
+
+const AWS = require('aws-sdk');
+
 
 export const get = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
-    const s3 = new S3Client("ap-southeast-1");
-    const { key, contentType = undefined, method = S3_METHODS.get }: any = event.queryStringParameters;
+    const { key, contentType = undefined }: any = event.queryStringParameters;
+    const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
-    // Set the parameters
-    const params = {
-      Bucket: process.env.S3_BUCKET_USER_DATA, // The name of the bucket. For example, 'sample-bucket-101'.
-      Key: "sample_upload.txt", // The name of the object. For example, 'sample_upload.txt'.
-      Body: "Test", // The content of the object. For example, 'Hello world!".
+    var params: any = {
+      Bucket: process.env.S3_BUCKET_USER_DATA,
+      Key: key,
+      Expires: 300,
     };
+    if (contentType) {
+        params.ContentType = contentType;
+    }
 
-    console.log("params :", params);
+    const url = await s3.getSignedUrl(S3_METHODS.put, params);
 
-    const results = await s3.send(new PutObjectCommand(params));
-
-    console.log("received queryStringParameters: ", key, contentType, method);
+    console.log("received queryStringParameters: ", key, contentType);
     // const url = await getSignedUrl(process.env.S3_BUCKET_USER_DATA, key, method, contentType);
     // console.log("url :", url);
-    return getSuccessResponse({ body: { results } });
+    return getSuccessResponse({ body: { url } });
   } catch (error) {
     return getErrorResponse(error.message);
   }
