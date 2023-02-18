4cdf60bf46a9e408faf2c4bb226837ee30485650
Dilshan Naveen <dilshan@vertex.lk>
2023-02-18 12:44:43 +0530

    added axios

--- hairstyle-recommender/handler.ts
+++ hairstyle-recommender/handler.ts
@@ -1,9 +1,11 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { getSuccessResponse, getErrorResponse } from "./utils/responseUtil";
+import axios from 'axios';
 
 export const get = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
-    return getSuccessResponse({ status: "SUCCESS", Test: `${process.env.S3_BUCKET_USER_DATA}` });
+    const result = await axios.get('https://dummy.restapiexample.com/api/v1/employees');
+    return getSuccessResponse({ status: "SUCCESS", Test: `${process.env.S3_BUCKET_USER_DATA}`, result });
   } catch (error) {
     return getErrorResponse(error.message);
   }
