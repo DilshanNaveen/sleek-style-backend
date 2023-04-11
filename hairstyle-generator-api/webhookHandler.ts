d8911a67a878796093798543c9172775b5d97f1e
Dilshan Naveen <dilshan@vertex.lk>
2023-04-12 02:57:06 +0530

    improved webhook

--- hairstyle-generator-api/webhookHandler.ts
+++ hairstyle-generator-api/webhookHandler.ts
@@ -1,8 +1,12 @@
 import { Handler } from "aws-lambda";
+import axios from "axios";
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
 type queryStringParameters = {
   id: string;
 }
+
+const s3Client = new S3Client({});
 
 export const post: Handler = async (event: any, res: any) => {
   try {
@@ -10,6 +14,23 @@ export const post: Handler = async (event: any, res: any) => {
     
     console.log("id :", id);
     console.log("event.body :", event.body);
+
+    const { status, output } = event.body;
+
+    console.log('status :', status);
+    console.log('output :', output);
+
+    const imageResponse = await axios.get(output, { responseType: 'arraybuffer' });
+    const imageData = imageResponse.data;
+
+    // Upload the image to S3
+    const putObjectCommand = new PutObjectCommand({
+      Bucket: process.env.S3_BUCKET_USER_DATA,
+      Key: `${id}/generated_image.png`,
+      Body: imageData,
+      ContentType: 'image/png'
+    });
+    await s3Client.send(putObjectCommand);
 
     res.sendStatus(200);
   } catch (error) {
