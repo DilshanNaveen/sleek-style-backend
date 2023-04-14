625224a58c432de17bac4927a0e2bfdbf2113ae7
DilshanNaveen <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>
2023-04-14 08:31:45 +0000

    updated the script

--- hairstyle-generator/download_pre_trained_models.sh
+++ hairstyle-generator/download_pre_trained_models.sh
@@ -1,10 +1,9 @@
+rm -rf trained_models
 mkdir trained_models
 cd trained_models
-gdown 137_9v9BS490fPSSr8BkyBqzGjTC9wAZO
-gdown 16v6jPtKVlvq8rg2Sdi3-R9qZEVDgvvEA
-gdown 1HXLER5R3EMI8DSYDBZafoqpX4EtyOf2R
-gdown 1AT6bNR2ppK8f2ETL_evT27f3R_oyWNHS
-gdown 16wM2PwVWzaMsRgPExvRGsq6BWw_muKbf
-gdown 1lIKvQaFKHT5zC7uS4p17O9ZpfwmwlS62
-gdown 1huhv8PYpNNKbGCLOaYUjOgR1pY5pmbJx
-# rm -rf trained_models.zip
+gdown 1CMapMlu2Y6zVF_TREUEf3Llv59RmQXgN # shape_predictor_68_face_landmarks.dat
+gdown 1CLDwW-h0L-ZZxI8cE-RSA9QTSgG589d2 # ffhq.pt
+gdown 15OH808sHDL0vawDk4tvTgcvB5dSH7HI9 # ffhq_PCA.npz
+# gdown 15BpwtaLMID2zXNcqbxoR_ZKR-OasipRS # metfaces_PCA.npz
+
+cd ../ && cog predict -i identity_image=@identity_image.png -i appearance_image=@appearance_image.png
