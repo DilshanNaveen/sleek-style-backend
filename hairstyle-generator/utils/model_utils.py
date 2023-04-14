c07d94cca4409a40a19d6fd97a6d0caa131b3095
DilshanNaveen <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>
2023-04-14 07:09:34 +0000

    fixed bug

--- hairstyle-generator/utils/model_utils.py
+++ hairstyle-generator/utils/model_utils.py
@@ -1,7 +1,6 @@
 import gdown
 import os
 
-
 weight_dic = {'afhqwild.pt': 'https://drive.google.com/file/d/14OnzO4QWaAytKXVqcfWo_o2MzoR4ygnr/view?usp=sharing',
                 'afhqdog.pt': 'https://drive.google.com/file/d/16v6jPtKVlvq8rg2Sdi3-R9qZEVDgvvEA/view?usp=sharing',
                 'afhqcat.pt': 'https://drive.google.com/file/d/1HXLER5R3EMI8DSYDBZafoqpX4EtyOf2R/view?usp=sharing',
