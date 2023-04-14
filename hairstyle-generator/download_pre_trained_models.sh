5a9a8993f446aca69451fa793e673884449d9ff8
DilshanNaveen <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>
2023-04-14 07:11:56 +0000

    added shell script

--- hairstyle-generator/download_pre_trained_models.sh
+++ hairstyle-generator/download_pre_trained_models.sh
@@ -1,3 +1,10 @@
-# gdown 137_9v9BS490fPSSr8BkyBqzGjTC9wAZO
-unzip trained_models.zip
+mkdir trained_models
+cd trained_models
+gdown 137_9v9BS490fPSSr8BkyBqzGjTC9wAZO
+gdown 16v6jPtKVlvq8rg2Sdi3-R9qZEVDgvvEA
+gdown 1HXLER5R3EMI8DSYDBZafoqpX4EtyOf2R
+gdown 1AT6bNR2ppK8f2ETL_evT27f3R_oyWNHS
+gdown 16wM2PwVWzaMsRgPExvRGsq6BWw_muKbf
+gdown 1lIKvQaFKHT5zC7uS4p17O9ZpfwmwlS62
+gdown 1huhv8PYpNNKbGCLOaYUjOgR1pY5pmbJx
 # rm -rf trained_models.zip
